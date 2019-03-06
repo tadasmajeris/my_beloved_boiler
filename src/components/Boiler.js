d9ce7bcb218881ac51410bc054f5503e0e2b12da
Tadas Majeris <tadas.majeris@gmail.com>
2019-03-06 10:10:11 +0000

    css, hovers

--- src/components/Boiler.js
+++ src/components/Boiler.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Boiler = ({boiler}) => (
-  <div>
-    <img src={`https://img.youtube.com/vi/${boiler.youtubeId}/0.jpg`} height='135'/>
+  <div className='boiler'>
+    <img src={`https://img.youtube.com/vi/${boiler.youtubeId}/0.jpg`} height='135' alt='thumb'/>
     <h1>{boiler.artist}</h1>
-    <h2>{boiler.location}</h2>
-    <h3>{boiler.year}</h3>
+    <h3>{boiler.location}</h3>
+    <h4>{boiler.year}</h4>
   </div>
 );
 
